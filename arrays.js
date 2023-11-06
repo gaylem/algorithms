80c08eba1d9cb70764030e6fd9f19654ba0f8940
Gayle Martin <hello.gmartin@gmail.com>
2023-11-06 16:50:01 -0600

    Create arrays.js file and complete Contains Duplicates challenge

---
+++ arrays.js
@@ -0,0 +1,44 @@
+/* Leetcode 217. Contains Duplicate - Easy
+https://leetcode.com/problems/contains-duplicate/description/ 
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+ 
+Constraints:
+
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+const containsDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
+  console.log(nums);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) return true;
+  }
+  return false;
+};
+
+const setSolution = function (nums) {
+  let testSet = new Set(nums);
+  return testSet.size !== nums.length;
+}
+
+nums = [1, 2, 3, 1];
+console.log(containsDuplicate(nums));
